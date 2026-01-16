6584326c174887f03207b50ce5d4655ac0c4fadc
shay0454 <shay0454@naver.com>
2026-01-17 01:09:29 +0900

    Auto stash before checking out "origin/main"

--- AvoidMeteoroid/main.cpp
+++ AvoidMeteoroid/main.cpp
@@ -1,10 +1,9 @@
 #include<iostream>
 #include<SDL.h>
 #include"Game.h"
-#include"AvoidAstroidGame.h"
 
 int main(int argc, char** argv) {
-	AvoidAstroidGame game;
+	Game game;
 	bool success = game.Initialize();
 	if (success) {
 		game.RunLoop();
